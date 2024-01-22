3942dd60c362fca703db0e532e1397dd9e2e9c48
Jimih Setiawan <setiaman.jimih@gmail.com>
2024-01-22 17:49:49 +0900

    Add files via upload

---
+++ src/pages/SyaratKetenPage.jsx
@@ -0,0 +1,58 @@
+import{Container, Row, Col} from "react-bootstrap";
+import FaqComponent from "../components/FaqComponent";
+
+
+const SyaratKetenPage = () => {
+  return (
+    <div className="syarat-ketentuan-page">
+     <div className="syarat-ketentuan min-vh-100">
+      <Container>
+        <Row className="pt-5">
+          <Col>
+          <h1 className="fw-bold text-center mb-2">Syarat & Ketentuan</h1>
+          <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat dolor minus nesciunt corrupti adipisci praesentium ab quam perferendis libero facere qui cumque soluta ducimus dolorem doloremque placeat architecto commodi tenetur optio quasi laborum, nulla cupiditate? Accusamus magnam eveniet quia velit, totam modi perferendis assumenda harum, reiciendis, consequuntur qui. Quos facere quam architecto veniam, voluptatibus 
+            voluptate ullam perspiciatis asperiores aperiam sed?</p>
+        </Col>
+        </Row>
+        <Row className="py-3">
+          <Col>
+          <h4 className="fw-bold">1. Lorem </h4>
+          <p>ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et repellat quibusdam consequatur?
+             Ducimus iusto excepturi voluptas! Illum, sapiente nisi.</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident officia aut quis iste? Illo totam incidunt vitae impedit sint. Debitis dolores consequatur quisquam cupiditate expedita libero, est repellat error velit beatae mollitia consectetur sed et illum atque dolorum rem? Laudantium molestias maxime nam incidunt ipsum magnam voluptatem eligendi, ratione perspiciatis ab rem dolore,
+             in culpa nostrum. Corrupti unde iusto tenetur!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eius eos quisquam, iure itaque repellat error facilis omnis voluptates eum voluptas minus velit rem voluptatem quis excepturi et ea nobis? Porro aperiam corrupti quo? Enim incidunt, ex quibusdam, error a commodi consequatur
+             nemo eaque praesentium illum aliquid doloremque porro odio?</p>   
+          </Col>
+        </Row>
+        <Row className="py-3">
+          <Col>
+          <h4 className="fw-bold">2. Lorem</h4>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi nobis aliquam rem, quas hic nemo obcaecati deserunt recusandae earum rerum eaque dolores, qui, veniam minus dolor maxime fugit voluptates. Quibusdam accusamus doloribus et alias, 
+            aspernatur impedit expedita. Numquam, mollitia obcaecati.</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet facere est neque itaque praesentium veniam necessitatibus magni, ut dolorem dolor modi, maxime assumenda asperiores explicabo, 
+            nam molestias quasi! Exercitationem, vel?</p>
+          </Col>
+        </Row>
+        <Row className="py-3">
+          <Col>
+          <h4 className="fw-bold">3. Lorem</h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos neque eveniet ipsum, eos porro quidem quo impedit quasi, eius harum corrupti ut et sequi incidunt ipsam blanditiis? Voluptatibus officiis cumque enim error aut eius quod tempore nesciunt a, debitis iusto corrupti amet possimus quibusdam architecto repellat quam velit dolore accusamus, autem fugit impedit? 
+            Saepe molestiae neque tempora earum! Quibusdam, laudantium.</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, fugiat culpa? Accusantium quibusdam unde quidem soluta? Soluta numquam ex itaque molestiae commodi quae voluptatem. Molestias suscipit eaque tempore doloribus praesentium obcaecati sit, adipisci sequi cumque modi reprehenderit, quasi, dolore cupiditate pariatur porro! Provident quo veritatis vel nobis nostrum! Excepturi, praesentium. Corporis rerum harum assumenda quisquam tenetur ad. Repellendus recusandae dolores et. Veniam temporibus exercitationem commodi, minus, adipisci aspernatur non iusto 
+            officiis at voluptate eaque sequi fugit ex omnis? Nihil, quod?</p>
+          </Col>
+        </Row>
+      </Container>
+     </div>
+     <FaqComponent/>
+    </div>
+  );
+}
+
+export default SyaratKetenPage;
